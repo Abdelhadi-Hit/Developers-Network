9c589263207baaf07a060d7499418c7489691ae9
ABDELHADI BAKHTY <abdelhadibakhty@ABDELHADIs-MacBook-Pro.local>
2024-01-15 23:54:14 +0100

    Connecting ...

---
+++ client/src/App.js
@@ -0,0 +1,61 @@
+import React, { Fragment, useEffect } from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Navbar from "./components/layouts/Navbar";
+import Landing from "./components/layouts/Landing";
+import Register from "./components/auth/Register";
+import Login from "./components/auth/Login";
+import Alert from "./components/layouts/Alert";
+import Dashboard from "./components/dashboard/Dashboard";
+import CreateProfile from "./components/profile-forms/CreateProfile";
+import EditProfile from "./components/profile-forms/EditProfile";
+import AddExperience from "./components/profile-forms/AddExperience";
+import AddEducation from "./components/profile-forms/AddEducation";
+import Profiles from "./components/profiles/Profiles";
+import Profile from "./components/profile/Profile";
+import PrivateRoute from "./components/routing/PrivateRoute";
+// Redux
+import { Provider } from "react-redux";
+import store from "./store";
+import { loadUser } from "./actions/auth";
+import setAuthToken from "./utils/setAuthToken";
+
+import "./App.css";
+
+if (localStorage.token) {
+  setAuthToken(localStorage.token);
+}
+
+const App = () => {
+  // the empty brackets passed in the parameters so that the useEfect only runs once
+  // it tells that our useEffect does not depend on any parameters it does not need to re-run
+  useEffect(() => {
+    store.dispatch(loadUser());
+  }, []);
+
+  return (
+    <Provider store={store}>
+      <Router>
+        <Fragment>
+          <Navbar />
+          <section>
+            <Alert />
+            <Routes>
+              <Route exact path="/" element={<Landing />} />
+              <Route exact path="/register" element={<Register />} />
+              <Route exact path="/login" element={<Login />} />
+              <Route exact path="/profiles" element={<Profiles />}/>
+              <Route exact path="/profile/:id" element={<Profile />}/>
+              <Route exact path="/dashboard" element={<Dashboard />} />
+              <Route exact path="/create-profile" element={<CreateProfile />} />
+              <Route exact path="/edit-profile" element={<EditProfile />} />
+              <Route exact path="/add-experience" element={<AddExperience />} />
+              <Route exact path="/add-education" element={<AddEducation />}/>
+            </Routes>
+          </section>
+        </Fragment>
+      </Router>
+    </Provider>
+  );
+};
+
+export default App;
